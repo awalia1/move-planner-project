b2963f779b709f0854138c7dc8ea55ae673906ea
amarpreet.s.walia@gmail.com <amarpreet.s.walia@gmail.com>
2017-12-29 16:01:10 -0500

    added .error

--- js/script.js
+++ js/script.js
@@ -33,7 +33,9 @@ function loadData() {
 				'<p>' + article.snippet + '</p>'+ 
 			'</li>');
 		};
-	})
+	}).error(function(e){
+			$nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
+	});
 	
 		return false;
 };
