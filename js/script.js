b30e9927c887919770f92e311942ac602a5c0f19
amarpreet.s.walia@gmail.com <amarpreet.s.walia@gmail.com>
2018-01-01 19:12:57 -0500

    added timeout.

--- js/script.js
+++ js/script.js
@@ -39,6 +39,10 @@ function loadData() {
 	
 	var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
 	
+	var wikiRequestTimeout = setTimeout (function(){
+		$wikiElem.text("failed to get wikipedia resources");
+	}, 8000);
+	
 	$.ajax(wikiUrl,{
 		dataType: "jsonp",
 		success: function (response){
@@ -50,6 +54,8 @@ function loadData() {
 				$wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
 			};
 			
+			clearTimeout(wikiRequestTimeout);
+		
 		}
 		
 	});
